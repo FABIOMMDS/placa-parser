bbb5000a9634147d2378c9ff25ce5a12589abc9f
FABIOMMDS <fabinnnmartins@hotmail.com>
2025-04-05 20:15:02 -0300

    Update consulta.js

--- consulta.js
+++ consulta.js
@@ -1,6 +1,6 @@
 const { getVehicleData } = require('./index');
 
-const placa = 'ABC1D23'; // Substitua por uma placa válida
+const placa = 'AIS4A09'; // Substitua por uma placa válida
 
 getVehicleData(placa).then(dados => {
   console.log('Dados do veículo:');
